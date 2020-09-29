813558fe11e1cb0cefc8a95052e72f600dccc310
SamyakJain091998 <2016224@iiitdmj.ac.in>
2020-09-29 12:01:25 +0530

    master initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Day-8-Assignment
+master branch!
---
+++ src/com/day_8/masterBranch.java
@@ -0,0 +1,10 @@
+package com.day_8;
+
+public class masterBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to address book!");
+	}
+
+}
